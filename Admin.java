98ace0f52946f2c855c9841c7cb164e2274fb8fd
kiruba49 <134125347+kiruba49@users.noreply.github.com>
2025-07-22 19:11:56 +0530

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,8 @@
+public class Admin {
+    private final String username = "admin";
+    private final String password = "admin123";
+
+    public boolean login(String user, String pass) {
+        return username.equals(user) && password.equals(pass);
+    }
+}
